1f62a543097e58ed77ee4836c84bddd00dc2389f
AliaTec <vbarragan@aliatec.com.mx>
2016-04-06 16:09:02 -0500

    Layouts Pagos
    Se agregaron dos layouts más

--- RMQ/Aliatec-ITX-RMQ/Dispersion/Items.vb
+++ RMQ/Aliatec-ITX-RMQ/Dispersion/Items.vb
@@ -11,11 +11,27 @@ Public Class Items
         Dim newRow As DataRow
 
         newRow = Me.NewRow()
+        newRow.Item("Label") = 100014
+        'accion
+        newRow.Item("Accion") = 100014
+        'case del aspx
+        newRow.Item("Case") = 100014
+        Me.Rows.Add(newRow)
+
+        newRow = Me.NewRow()
         newRow.Item("Label") = 100015
         'accion
         newRow.Item("Accion") = 100015
         'case del aspx
         newRow.Item("Case") = 100015
+        Me.Rows.Add(newRow)
+
+        newRow = Me.NewRow()
+        newRow.Item("Label") = 100016
+        'accion
+        newRow.Item("Accion") = 100016
+        'case del aspx
+        newRow.Item("Case") = 100016
         Me.Rows.Add(newRow)
 
         newRow = Me.NewRow()
